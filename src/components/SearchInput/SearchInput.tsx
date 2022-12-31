b4a210c6bfa3e8f77ee214aaa9ae205b2666c10d
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2022-12-31 13:48:11 -0800

    added debouncing function

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -5,21 +5,27 @@ import Autocomplete from "@mui/material/Autocomplete";
 import { useState, useEffect } from "react";
 import { getCommunitiesByTerm } from "../../../lib/models/community/queries";
 import { Community } from "../../../lib/models/community/Community";
+import debounce from "lodash.debounce";
 
 export default function SearchInput() {
   const [userInput, setUserInput] = useState<string>("");
   const [totalOptions, setTotalOptions] = useState<Community[]>([]);
 
-  const onInputChange = async (e: any) => {
-    if (!e.target.value) {
+  const deboucedValue = debounce((value) => {
+    onInputChange(value);
+  }, 400);
+
+  const onInputChange = async (value: string) => {
+    if (!value) {
+      setTotalOptions([]);
       return;
     }
 
-    setUserInput(e.target.value);
+    setUserInput(value);
 
     // make api call
 
-    const communities = await getCommunitiesByTerm(e.target.value);
+    const communities = await getCommunitiesByTerm(value);
 
     // once succesful, setState for new options
 
@@ -29,11 +35,13 @@ export default function SearchInput() {
   return (
     <Stack>
       <Autocomplete
-        options={totalOptions.map((option) => option.community_name)}
+        options={totalOptions.map(
+          (option) => `${option.community_name}: ${option.caption}`
+        )}
         renderInput={(params) => {
           return <TextField {...params} />;
         }}
-        onInputChange={onInputChange}
+        onInputChange={(e: any) => deboucedValue(e.target.value)}
         clearOnBlur={false}
         clearOnEscape={false}
         noOptionsText="No Communities found"
